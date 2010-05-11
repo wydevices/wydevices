c799d4bc10d100c286cbc3e3ecd41de4fc6a25e7
roberto.clemente.bustamante <roberto.clemente.bustamante@968cd671-5f78-0f86-1f23-c28cc3e12018>
2010-05-11 16:38:53 +0000

    Reorganization. Guidebugmode integrated. Added a switch to select through extras form's

---
+++ wymodCP/compileSH4wymoddaemon.sh
@@ -0,0 +1,1 @@
+/ZTV/opt/crosstool/gcc-4.1.1-glibc-2.6.1/sh4-unknown-linux-gnu/bin/sh4-unknown-linux-gnu-gcc-4.1.1 -Wall /ZTV/subversion/wymodCP/source/wymod.c /ZTV/subversion/wymodCP/source/mongoose.c -ldl -lpthread -o /ZTV/subversion/wymodCP/wymoddaemon
---
+++ wymodCP/source/compileSH4wymoddaemon.sh
@@ -0,0 +1,1 @@
+/ZTV/opt/crosstool/gcc-4.1.1-glibc-2.6.1/sh4-unknown-linux-gnu/bin/sh4-unknown-linux-gnu-gcc-4.1.1 -Wall wymod.c mongoose.c -ldl -lpthread -o /ZTV/subversion/wymodCPv1.6/wymoddaemon
