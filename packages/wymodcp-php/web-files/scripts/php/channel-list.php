5fba7b060cbb0c0ef55b14b4f29d4d7ddd51dc47
roberto.clemente.bustamante@gmail.com <roberto.clemente.bustamante@gmail.com@968cd671-5f78-0f86-1f23-c28cc3e12018>
2010-08-08 19:36:02 +0000

    This is a combo box for channel reordering mechanism

--- packages/wymodcp-php/web-files/scripts/php/channel-list.php
+++ packages/wymodcp-php/web-files/scripts/php/channel-list.php
@@ -1,7 +1,10 @@
 <html>
 <head><script src="../js/wydev.js" type="text/javascript"></script></head>
 <body>
-<table>
+<form id=channel action="channelform.php" method="put">
+
+<select id="channel" name="channel"> 
+
 <?php
 	$dbh = new PDO('sqlite:/etc/params/wyscan/wyscan.db');
 	$sql = 'SELECT LOGICAL_CHANNEL_NUMBER, NAME FROM T_SERVICE ORDER BY LOGICAL_CHANNEL_NUMBER ASC';
@@ -11,21 +14,27 @@
 		$column = $id%"3";
 		$channel = $row['NAME'];
 
-		switch ($column) {
-		    case 0: echo "<td>".$channel."</td></tr>";
-				break;
-		    case 1: echo "<tr><td>".$channel."</td>";
-				break;
-		    case 2: echo "<td>".$channel."</td>";
-				break;
-		    default: echo "default";
-				break;
-		}
-
+		echo "<option value=\"".$id."\" >".$channel."</option> ";
 	}
+
 ?>
 
+</select>
+<br>
+<hr>
+<label> New Order: </label>
+<input id="neworder" name="neworder" class="text" type="text" maxlength="2" value=""/>
+<br>
+<label> New Name: </label>
+<input id="newname" name="newname" class="text" type="text" maxlength="14" value=""/>  
+<br>
+
+
+<input type=submit></input>
 
 </table>
 </body>
 </html>
+
+
+
