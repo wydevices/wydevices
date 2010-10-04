1a8c9d21f60778404b8ac0cdb4e597348fb83efc
roberto.clemente.bustamante <roberto.clemente.bustamante@968cd671-5f78-0f86-1f23-c28cc3e12018>
2010-10-04 09:29:44 +0000

    fixed temp output, stills a unknown showbug on hddtemp...

--- packages/wymodcp-php/web-files/scripts/php/home.php
+++ packages/wymodcp-php/web-files/scripts/php/home.php
@@ -14,9 +14,9 @@
 </td></tr>
 <tr><td>Serial Number: </td><td> <?php system("cat /proc/wybox/SN")?> </td></tr>
 <tr><td>Time / Uptime: </td><td> <?php system("uptime |cut -f1 -d,")?> </td></tr>
-<tr><td>Fan Speed: </td><td> <?php system("temp |cut -f2 -d: |cut -f1")?> </td></tr>
-<tr><td>Cpu Temp: </td><td> <?php system("temp |cut -f3 -d: |cut -f1")?> </td></tr>
-<tr><td>HD Temp: </td><td> <?php system("temp |cut -f4 -d: |cut -f1")?> </td></tr>
+<tr><td>Fan Speed: </td><td> <?php system("cat /sys/devices/platform/stm-pwm/pwm1")?> </td></tr>
+<tr><td>HD Temp: </td><td> <?php system("hddtemp -q -n /dev/sda -f /wymedia/usr/lib/share/misc/hddtemp.db")?> </td></tr>
+<tr><td>Cpu Temp: </td><td> <?php system("cat /sys/bus/i2c/devices/0-0048/temp1_input")?> </td></tr>
 <tr><td>dm-0 Slave: </td><td> <?php system("ls /sys/block/dm-0/slaves")?> </td></tr>
 <tr><td>Board: </td><td> <?php system("cat /proc/fb |cut -c3-")?> </td></tr>
 <tr><td>Target: </td><td> <?php system("strings /dev/mtd2 |grep WC |cut -d= -f2")?> </td></tr>
