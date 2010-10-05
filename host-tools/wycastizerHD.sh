9f618eb23968856c87c4977926ab68db0f5e4513
roberto.clemente.bustamante <roberto.clemente.bustamante@968cd671-5f78-0f86-1f23-c28cc3e12018>
2010-10-05 08:27:18 +0000

    added 2 conversion host-tools, decomposition of wycastizerHD.sh

--- host-tools/wycastizerHD.sh
+++
@@ -1,3 +0,0 @@
-mencoder $1 -ovc xvid -oac mp3lame -xvidencopts pass=1 -o /tmp/out.avi
-ffmpeg -i /tmp/out.avi -s 640x480 -an -vcodec libx264 -vpre normal -pass 1 -padleft 26 -padright 26 -padtop 14 -padbottom 14 $2
-
