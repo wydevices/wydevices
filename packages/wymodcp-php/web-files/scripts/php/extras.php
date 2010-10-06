1f5f85b887803701444a0ed78955e405b5b66b69
roberto.clemente.bustamante <roberto.clemente.bustamante@968cd671-5f78-0f86-1f23-c28cc3e12018>
2010-10-06 13:50:04 +0000

    added <?php header("Cache-Control: no-cache"); ?> to avoid page caching, bugfix for incorrect picture output,

--- packages/wymodcp-php/web-files/scripts/php/extras.php
+++ packages/wymodcp-php/web-files/scripts/php/extras.php
@@ -1,3 +1,6 @@
+<?php
+header("Cache-Control: no-cache");
+?>
 <h2> Extras Management </h2>
 <form id="extrasform" method="get" action="./scripts/php/extrashandler.php">
 	<div class="form_description"><p>Update extras configuration</p></div>
