529f9c98e6ace4642f5bea2bed6d5a351105a01d
argos66@gmail.com <argos66@gmail.com@968cd671-5f78-0f86-1f23-c28cc3e12018>
2010-10-14 16:37:27 +0000

    - Add a TODO list. - Add visibility for download don't exist in myrecords.fxd. - Complete list of case for record status. - Fix a bug in record duration calcul.

--- packages/wymodcp-php/web-files/scripts/php/records.php
+++ packages/wymodcp-php/web-files/scripts/php/records.php
@@ -1,20 +1,38 @@
 <?php
+/*
+ * This file is part of Wydevices project, http://code.google.com/p/wydevices
+ * 
+ * Wydevices record utility
+ * 
+ * Author : Argos <argos66@gmail.com>
+ * 
+ * TODO :
+ *    - Add a cleaner for myrecords.fxd (In conflict record, Rules not linked, Record without video content, etc ...)
+ *    - Group records are in the same schedule rule and add a possibility to expand this group.
+ *    - Schedule a single record.
+ *    - Schedule a periodic record.
+ * 
+ */
 
-// Path of the myrecords.fxd file
-$myrecord_path = "/wymedia/timeshift/";
-//$myrecord_path = "/var/www/test/record/"; // Test
+$myrecord_path = "/wymedia/timeshift/"; //Path of the myrecords.fxd file
+$record_path   = $myrecord_path."records/"; //Path of records
 
-//Download a record (merge capability for all chunks)
-if (isset($_GET['name']) && !empty($_GET['name']) && isset($_GET['path']) && !empty($_GET['path'])){
-    $record_file_dir = $_GET['path'];
+
+/*
+ * Download a record
+ */
+if (isset($_GET['name']) && !empty($_GET['name']) && isset($_GET['path']) && !empty($_GET['path'])) {
+    $record_file_dir = $record_path.$_GET['path']."/";
     $filename = $_GET['name'];
 
-    if ($handle_record_file_dir = opendir($record_file_dir)) {
+    if (file_exists($record_file_dir)) {
         unset($record_chunk_filepath);
         $record_filesize = 0;
         $i = 0;
 
-        //List of chunks to merge into an array
+        $handle_record_file_dir = opendir($record_file_dir);
+
+        //List all chunks of one record into an array
         while (false !== ($record_file = readdir($handle_record_file_dir))) {
             if ($record_file != "." && $record_file != ".." && strstr($record_file, '.ts')) {
                 $record_filesize = $record_filesize + filesize($record_file_dir.$record_file);
@@ -44,45 +62,48 @@ if (isset($_GET['name']) && !empty($_GET['name']) && isset($_GET['path']) && !em
 
     set_time_limit(0); //Stop timeout counter
 
-    //Loop to merge all chunks to buffer
-    for($r = 0; $r < $i; $r++){
+    /*
+     * Warning     : At this time, all error will be logged into the .ts file ! (use a Hex editor for check error return)
+     * Warning     : This download code don't support browser pause or reprise.
+     * Information : Chunk max size = 20MB (Number of seconds per chunk depend of AV compression ratio)
+     */
+
+    //Loop each chunks to buffer
+    for ($r = 0; $r < $i; $r++) {
         $chunk_size = filesize($record_chunk_filepath[$r]);
-        if (file_exists($record_chunk_filepath[$r])){
-            $chunk_handle = fopen($record_chunk_filepath[$r], 'r') or die("Error opening file");
-            while(!feof($chunk_handle)){
-                print fread($chunk_handle, $chunk_size) or die("Error reading file");
-                flush();
-                @ob_flush();
-            }
-            fclose($record_chunk_filepath[$r]);
-        }else{
-            echo "File : ".$record_chunk_filepath[$r]." doesn't exist";
-            exit;
+        $chunk_handle = fopen($record_chunk_filepath[$r], 'r') or die("Error opening file");
+
+        //Loop for download one chunk
+        while (!feof($chunk_handle)) {
+            print fread($chunk_handle, $chunk_size) or die("Error reading file");
+            flush(); //Free buffers
         }
+        fclose($record_chunk_filepath[$r]);
     }
     exit;
 }
 
-// Load myrecords.fxd file, this files contain scheduled record
-if (!file_exists($myrecord_path."myrecords.fxd")) {
-	echo "XML record file doesn't exist (".$myrecord_path."myrecords.fxd)\n";
-	exit;
-} else {
-	if($xml_records=simplexml_load_file($myrecord_path."myrecords.fxd")) {
-		$nb_rules = count($xml_records->periodicity->rule);
-		$nb_record = count($xml_records->recordings->recording);
-		$last_saved = $xml_records->general['save_time'];
 
-		for ($p = 0; $p < $nb_rules; $p++) { // Get periodic name
-			$periodicity = $xml_records->periodicity->rule[$p];
-			//$record_period_id[$p] 	= $periodicity->recording_ref['periodicity_rule_id']; // Get perdiocity ID
-			$record_periodicity[$p] = $periodicity->extern_name; // Get perdiocity name (daily, weekly, etc ...)
-			//$record_period_name[$p] = $periodicity->recording_ref['name']; // Get name of the periodic record
-		}
-	} else {
-		trigger_error("Error reading XML file", E_USER_ERROR);
-		exit;
-	}
+/*
+ * Load myrecords.fxd file, this files contain scheduled record
+ */
+if (!file_exists($myrecord_path."myrecords.fxd")) {
+    echo "XML record file doesn't exist (".$myrecord_path."myrecords.fxd)\n";
+    exit;
+} else {
+    if($xml_records=simplexml_load_file($myrecord_path."myrecords.fxd")) {
+        $nb_rules = count($xml_records->periodicity->rule);
+        $nb_record = count($xml_records->recordings->recording);
+        $last_saved = $xml_records->general['save_time'];
+
+        for ($p = 0; $p < $nb_rules; $p++) { //Get periodic name
+            $periodicity = $xml_records->periodicity->rule[$p];
+            $record_periodicity[$p] = $periodicity->extern_name; //Get perdiocity name (daily, weekly, etc ...)
+        }
+    } else {
+        trigger_error("Error reading XML file", E_USER_ERROR);
+        exit;
+    }
 
     echo "There are ".$nb_record." records.<br />\n";
     echo "There are ".$nb_rules." periodicity rules.<br />\n";
@@ -98,68 +119,118 @@ if (!file_exists($myrecord_path."myrecords.fxd")) {
             \t<td><b>Periodicity</b></td>\n
         </tr>\n";
 
-	// Loop into myrecords.fxd for each record and get informations
-	foreach ($xml_records->recordings->recording as $recording) {
-        // Variable initialization
+    $r = 0;
+    $record_file_dir = "";
+
+    //Loop into myrecords.fxd for each record and get informations
+    foreach ($xml_records->recordings->recording as $recording) {
         $record_file_info = "";
-        $record_file_dir = "";
         $record_size = 0;
         $record_size_mb = "";
         $record_periodic = 0;
 
-        // Get value from XML and convert it if necessary
-		$record_periodic_id = $recording['periodicity_rule_id'];
-		$record_name = $recording['name'];
-		$record_start_time = date("Y-m-d H:i:s", intval($recording['start_time'] - $recording['start_padding']));
-		$record_duration = date("H:i:s", intval($recording['stop_time'] - $recording['stop_padding'] - $recording['start_time'] - $recording['start_padding'] - 3600));
-		$record_channel = $recording->service['name'];
-		$record_file_info = $recording->file_info['file'];
-		$record_status = $recording['status'];
-		/* status :
-		 * 1 = scheduled
-         * 2 = in progress
-		 * 3 = exceeded / expired
-		 * 4 = recorded
-		 */
-
-		//Uncomment for force value to test
-		//$record_file_info = $myrecord_path."records/REC_20100322_204057_1_36258114307590_TMC_10/record.xml";
+        //Get value from XML and convert it if necessary
+        $record_periodic_id = $recording['periodicity_rule_id'];
+        $record_name = $recording['name'];
+        $record_start_time = date("Y-m-d H:i:s", intval($recording['start_time'] - $recording['start_padding']));
+        $record_duration = date("H:i:s", intval($recording['stop_time'] - $recording['start_time'] - 3600));
+        $record_channel = $recording->service['name'];
+        $record_file_info = $recording->file_info['file'];
+        $record_status = $recording['status'];
 
         //Get the record size into the record.xml
-		if (file_exists($record_file_info)) {
-			if($xml_record=simplexml_load_file($record_file_info)) {
-                $record_size = $xml_record->record[0]->information->size;
-                $record_size_kb = substr($record_size, 0, strlen($record_size) - 3); // Get only KB size
-				$record_size_mb = round($record_size_kb / 1048576,1); // Record size in MB
-				$record_size_mb .= " GB";
-			}
+        if (file_exists($record_file_info) && false !== ($xml_record=simplexml_load_file($record_file_info))) {
+            $record_size = $xml_record->record[0]->information->size;
+            $record_size_kb = substr($record_size, 0, strlen($record_size) - 3); //Get only KB size
+            $record_size_mb = round($record_size_kb / 1048576,1); //Record size in MB
+            $record_size_mb .= " GB";
 
-            $record_file_dir = str_replace("record.xml", "", $record_file_info);
-            $record_name = "<a href=\"scripts/php/records.php?path=".$record_file_dir."&amp;name=".$record_name."\">".$record_name."</a>\n";
-		}
+            //Get only record directory name
+            $record_file_dir[$r] = str_replace("record.xml", "", $record_file_info);
+            $record_file_dir[$r] = str_replace($record_path, "", $record_file_dir[$r]);
+            $record_file_dir[$r] = str_replace("/", "", $record_file_dir[$r]);
 
-        switch($record_status){
-            case 1 : $record_status = "scheduled"; break;
-            case 2 : $record_status = "in progess"; break;
-            case 3 : $record_status = "expired"; break;
-            case 4 : $record_status = "recorded"; break;
-            default: break;
+            $record_name = "<a href=\"scripts/php/records.php?path=".$record_file_dir[$r]."&amp;name=".$record_name."\">".$record_name."</a>\n";
+            $r++;
         }
 
-		echo "<tr>\n
-				\t<td>".$record_start_time."</td>\n
-				\t<td>".$record_channel."</td>\n
-				\t<td>".$record_name."</td>\n
-				\t<td>".$record_duration."</td>\n
-				\t<td>".$record_size_mb."</td>\n
-				\t<td>".$record_status."</td>\n";
-		if ($record_periodic_id > 0) {
-			echo "\t<td>".$record_periodicity[$record_periodic_id - 1]."</td>\n";
-		} else {
-			echo "\t<td></td>\n";
-		}
-		echo "</tr>\n";
-	}
-	echo "</table>\n";
+        switch($record_status){
+            case 0  : $record_status = "Unknown"; break;
+            case 1  : $record_status = "Scheduled"; break;
+            case 2  : $record_status = "Running"; break;
+            case 3  : $record_status = "In conflict"; break;
+            case 4  : $record_status = "Completed"; break;
+            case 5  : $record_status = "Canceled"; break;
+            case 6  : $record_status = "Missed"; break;
+            case 7  : $record_status = "Aborted"; break;
+            case 8  : $record_status = "Macrovision"; break;
+            case 9  : $record_status = "Disk space error"; break;
+            case 10 : $record_status = "System failure"; break;
+            default : $record_status = "Unknown"; break;
+        }
+
+        echo "<tr>\n
+                \t<td>".$record_start_time."</td>\n
+                \t<td>".$record_channel."</td>\n
+                \t<td>".$record_name."</td>\n
+                \t<td>".$record_duration."</td>\n
+                \t<td>".$record_size_mb."</td>\n
+                \t<td>".$record_status."</td>\n";
+        if ($record_periodic_id > 0) {
+            echo "\t<td>".$record_periodicity[$record_periodic_id - 1]."</td>\n";
+        } else {
+            echo "\t<td></td>\n";
+        }
+        echo "</tr>\n";
+    }
+
+
+/*
+ * List all records not referenced into myrecords.fxd
+ */
+    if ($handle_record_path = opendir($record_path)) {
+        $nb_record_dir = count($record_file_dir);
+
+        //List all record directory not existing into myrecords.fxd
+        while (false !== ($record_dir = readdir($handle_record_path))) {
+            $record_dir_match = 0;
+
+            if ($record_dir != "." && $record_dir != "..") {
+                for ($i = 0; $i <= $nb_record_dir; $i++) { if ($record_dir == $record_file_dir[$i]) $record_dir_match = 1; }
+                $record_file_info = $record_path.$record_dir."/record.xml";
+            }
+            if (!$record_dir_match && file_exists($record_file_info) && false !== ($xml_record=simplexml_load_file($record_file_info))) {
+                $record_size = $xml_record->record[0]->information->size;
+                $record_size_kb = substr($record_size, 0, strlen($record_size) - 3); //Get only KB size
+                $record_size_mb = round($record_size_kb / 1048576,1); //Record size in MB
+                $record_size_mb .= " GB";
+
+                $record_start_time = date("Y-m-d H:i:s", intval($xml_record->record[0]->information->start_time));
+                $record_channel = $xml_record->record[0]->information->channel;
+                $record_name = $xml_record->record[0]->information->name;
+                $record_duration = date("H:i:s", intval($xml_record->record[0]->information->stop_time - $xml_record->record[0]->information->start_time));
+                $record_status = "";
+
+                //Get only record directory name
+                $record_file_path = str_replace("record.xml", "", $record_file_info);
+                $record_file_path = str_replace($record_path, "", $record_file_path);
+                $record_file_path = str_replace("/", "", $record_file_path);
+
+                $record_name = "<a href=\"scripts/php/records.php?path=".$record_file_path."&amp;name=".$record_name."\">".$record_name."</a>\n";
+
+                echo "<tr>\n
+                        \t<td>".$record_start_time."</td>\n
+                        \t<td>".$record_channel."</td>\n
+                        \t<td>".$record_name."</td>\n
+                        \t<td>".$record_duration."</td>\n
+                        \t<td>".$record_size_mb."</td>\n
+                        \t<td>".$record_status."</td>\n
+                        \t<td></td>\n</tr>\n";
+            }
+        }
+        closedir($handle_record_path);
+    }
+
+    echo "</table>\n";
 }
 ?>
