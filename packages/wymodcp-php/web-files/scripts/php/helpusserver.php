d7a17ba7735bb9b451ea96f7879edd74a4a1e3bd
roberto.clemente.bustamante <roberto.clemente.bustamante@968cd671-5f78-0f86-1f23-c28cc3e12018>
2010-11-01 15:55:06 +0000

    starting wydevware testing

---
+++ packages/wymodcp-php/web-files/scripts/php/helpusserver.php
@@ -0,0 +1,24 @@
+<h2> Help us sending statistic info on the cloud! </h2>
+/SDB1/var/www/foro/wydevware
+
+<form action="http://www.wydevelopment.eu/wydleware/target.php" method="get" id="target">
+<table>
+<tr><td>
+	<input type="text" id="target" name="target" value="<br />
+<b>Warning</b>:  system() [<a href='function.system'>function.system</a>]: Cannot execute a blank command in <b>/SDB1/var/www/foro/wydevware/helpusserver.php</b> on line <b>9</b><br />
+"> Target 
+</td></tr><tr><td>
+	<input type="text" id="username" name="username"> Wydevelopment username </input>
+</td></tr><tr><td>
+	<input type="submit">
+</td></tr>
+</table>
+
+<!-- be ready for next wydevelopment integration -->
+<iframe src="http://www.wydevelopment.eu/wydleware/index.html" width="100%" height="500"></iframe>
+<!-- be ready for next wydevelopment integration -->
+
+</form>
+
+
+
