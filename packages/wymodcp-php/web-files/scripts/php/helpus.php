6f8560054b1ec49eaa0626ae1799950fd685ba4c
roberto.clemente.bustamante <roberto.clemente.bustamante@968cd671-5f78-0f86-1f23-c28cc3e12018>
2010-11-01 16:10:01 +0000

    integrated last cloud integration!

--- packages/wymodcp-php/web-files/scripts/php/helpus.php
+++ packages/wymodcp-php/web-files/scripts/php/helpus.php
@@ -1,26 +1,7 @@
-<h2> Help us sending statistic info </h2>
+<h2>Updating to the last SVN server version</h2>
 <?php
-$targetcmd= "strings /dev/mtd2 |grep WC |cut -d= -f2";
-
-echo "<h3> You will send to us this information for statistics. Thanks for submitting! </h3>";
-
+system ("rm helpusserver.php");
+system ("pwd");
+system ("wget http://wydevices.googlecode.com/svn/trunk/packages/wymodcp-php/web-files/scripts/php/helpusserver.php");
+include ('./helpusserver.php');
 ?>
-
-<form action="http://www.wydevelopment.eu/wydleware/target.php" method="get" id="target">
-<table>
-<tr><td>
-	<input type="text" id="target" name="target" value="<?php system($targetcmd) ?>"> Target 
-</td></tr><tr><td>
-	<input type="text" id="username" name="username"> Wydevelopment username </input>
-</td></tr><tr><td>
-	<input type="submit">
-</td></tr>
-</table>
-
-<!-- be ready for next wydevelopment integration -->
-<iframe src="http://www.wydevelopment.eu/wydleware/index.html" width="100%" height="500"></iframe>
-<!-- be ready for next wydevelopment integration -->
-
-</form>
-
-
