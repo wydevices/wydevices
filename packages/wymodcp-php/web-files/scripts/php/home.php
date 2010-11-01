337c801ad33f25cc2cad50103a4a7458a45f47d1
roberto.clemente.bustamante <roberto.clemente.bustamante@968cd671-5f78-0f86-1f23-c28cc3e12018>
2010-11-01 11:56:48 +0000

    show firmware version at homepage

--- packages/wymodcp-php/web-files/scripts/php/home.php
+++ packages/wymodcp-php/web-files/scripts/php/home.php
@@ -6,18 +6,15 @@
 
 <h2> Home </h2>
 <table> 
-<tr><td>Manufacturer: </td><td><b><?php system("cat /proc/wybox/MN")?>
-
-
-</b></td></tr>
-<tr><td>Modded Target: </td><td><?php system("cat /proc/wybox/WC")?>
-</td></tr>
+<tr><td>Manufacturer: </td><td><b><?php system("cat /proc/wybox/MN")?></td></tr>
+<tr><td>WydevFirm Version: </td><td><b><?php system("cat /wymedia/usr/etc/wydev-mod-version")?></td></tr>
+<tr><td>Modded Target: </td><td><?php system("cat /proc/wybox/WC")?></td></tr>
+<tr><td>Real Target: </td><td> <?php system("strings /dev/mtd2 |grep WC |cut -d= -f2")?> </td></tr>
 <tr><td>Serial Number: </td><td> <?php system("cat /proc/wybox/SN")?> </td></tr>
 <tr><td>Time / Uptime: </td><td> <?php system("uptime |cut -f1 -d,")?> </td></tr>
 <tr><td>Fan Speed & Temperatures: </td><td> <?php system("/wymedia/usr/bin/temp") ?> </td></tr>
 <tr><td>dm-0 Slave: </td><td> <?php system("ls /sys/block/dm-0/slaves")?> </td></tr>
 <tr><td>Board: </td><td> <?php system("cat /proc/fb |cut -c3-")?> </td></tr>
-<tr><td>Real Target: </td><td> <?php system("strings /dev/mtd2 |grep WC |cut -d= -f2")?> </td></tr>
 <tr><td>Net Settings: </td><td> <?php system("ifconfig | grep inet |grep -v 127.0.0.1 |cut -d: -f2-4")?></td></tr>
 <tr><td>Route table: </td><td><pre> <?php system("route")?></pre></td></tr>
 
