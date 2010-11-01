7549293bc37a1b12affff87f6cc56314b9253229
roberto.clemente.bustamante <roberto.clemente.bustamante@968cd671-5f78-0f86-1f23-c28cc3e12018>
2010-11-01 20:25:45 +0000

    updated path to update

--- packages/wymodcp-php/web-files/scripts/php/helpusserver.php
+++ packages/wymodcp-php/web-files/scripts/php/helpusserver.php
@@ -12,7 +12,7 @@
   <body>
         <img src="http://foro.wydev.es/download/file.php?avatar=54_1284403959.png"></img>
         <h1><strong>News:</strong></h1><h3> Last available firmware: <a href="http://foro.wydev.es/viewtopic.php?f=20&t=366">3.1</a><br>
-         Last update released version: <a href="http://foro.wydev.es/download/file.php?id=183">r249</a></h3><b>R249 Description:</b>
+         Last update released version: <a href="./updates/R249.tar.gz">r249</a></h3><b>R249 Description:</b>
 
 <small>Corrected TV Channel bugfixes and added improvements. Amazing records control panel, more info at home, new cloud wydevware integration... stills working in progress!!</small>
 <br>    <a href="http://foro.wydev.es/wydevware/ViewTargetsStatistics.php"> View Statistics! </a>
