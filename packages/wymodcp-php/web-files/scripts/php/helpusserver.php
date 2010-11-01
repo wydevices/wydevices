e6d8052872d048b1dc7244527212d1d8086a5eed
roberto.clemente.bustamante <roberto.clemente.bustamante@968cd671-5f78-0f86-1f23-c28cc3e12018>
2010-11-01 16:04:21 +0000

    fixed targetcmd

--- packages/wymodcp-php/web-files/scripts/php/helpusserver.php
+++ packages/wymodcp-php/web-files/scripts/php/helpusserver.php
@@ -1,23 +1,19 @@
-<h2> Help us sending statistic info on the cloud! </h2>
-<?php
-system("pwd");
-system("hostname");
-system("strings /dev/mtd2");
-?>
+<h2>Welcome to the cloud Enviroment</h2>
+<?php $targetcmd= "strings /dev/mtd2 |grep WC |cut -d= -f2"; ?>
 
-<form action="http://www.wydevelopment.eu/wydleware/target.php" method="get" id="target">
+<form action="http://foro.wydev.es/wydevware/target.php" method="get" id="target">
 <table>
 <tr><td>
         <input type="text" id="target" name="target" value="<?php system($targetcmd) ?>"> Target
 </td></tr><tr><td>
-        <input type="text" id="username" name="username"> Wydevelopment username </input>
+        <input type="text" id="username" name="username"> Wydev's forum username </input>
 </td></tr><tr><td>
         <input type="submit">
 </td></tr>
 </table>
 
 <!-- be ready for next wydevelopment integration -->
-<iframe src="http://www.wydevelopment.eu/wydleware/index.html" width="100%" height="500"></iframe>
+<iframe src="foro.wydev.es/wydevware/index.php" width="100%" height="500"></iframe>
 <!-- be ready for next wydevelopment integration -->
 
 </form>
