e8384bb3605e40a5e5a3467dbbb1d46f23076c83
roberto.clemente.bustamante <roberto.clemente.bustamante@968cd671-5f78-0f86-1f23-c28cc3e12018>
2010-11-02 09:04:25 +0000

    current, downloaded and latest mechanism

--- packages/wymodcp-php/web-files/scripts/php/helpusserver.php
+++ packages/wymodcp-php/web-files/scripts/php/helpusserver.php
@@ -1,7 +1,7 @@
 		<br>
         <h1><strong>News:</strong></h1>
-		<h3> Last available firmware: <a href="http://foro.wydev.es/viewtopic.php?f=20&t=366">3.1</a><br></h3>
-         <h3>Last update released version: <a href="http://foro.wydev.es/wydevware/updates/r249.tar.gz">r249</a></h3>
+		Last available firmware: <a href="http://foro.wydev.es/viewtopic.php?f=20&t=366">3.1</a><br>
+        Last update released version: <a href="http://foro.wydev.es/wydevware/updates/r249.tar.gz">r249</a>
 		 
 		 <b>R249 Description:</b>
 		 <small>Corrected TV Channel bugfixes and added improvements. Amazing records control panel, more info at home, new cloud wydevware integration... stills working in progress!!</small>
@@ -9,19 +9,20 @@
         <h1> </h1>
 <?php 
 
-system("cat /wymedia/usr/etc/wydev-mod-version");
+$downloaded = system("cat /wymedia/usr/share/updates/wydev-mod-updaterelease");
 $current = system("cat /wymedia/usr/etc/wydev-mod-updaterelease");
 system ("wget http://foro.wydev.es/wydevware/updates/latest.txt -q");
 $latest = system ("cat latest.txt");
 
-if ($current != $latest) {
+if ($downloaded != $latest) {
 	system ("wget http://foro.wydev.es/wydevware/updates/latest.tar.gz -q && mv latest.tar.gz /wymedia/usr/share/updates/");
 	echo "<script src='./scripts/js/wydev.js' type='text/javascript'></script>";
 	echo "<table><tr><td><button onclick='updatefromlocal()'></td><td><b>Update from /wymedia/usr/share/updates/</b></td></tr></table>";
-	system ("mv latest.txt /wymedia/usr/etc/wydev-mod-updaterelease");
+	system ("mv latest.txt /wymedia/usr/share/updates/wydev-mod-updaterelease");
 	}
 else {
-	echo ("System up to date");
+	 system("cat /wymedia/usr/share/updates/wydev-mod-updaterelease");
+	 echo ("as latest available");
 }
 
 ?>
--- packages/wymodcp-php/web-files/scripts/php/update.php
+++ packages/wymodcp-php/web-files/scripts/php/update.php
@@ -1,7 +1,7 @@
 <pre>
 <?php
-system ("tar -zxvf /wymedia/usr/share/updates/* -C /wymedia/");
-
+system ("tar -zxvf /wymedia/usr/share/updates/latest.tar.gz -C /wymedia/");
+system ("mv latest.txt /wymedia/usr/etc/wydev-mod-updaterelease");
 echo ("<b><h2> You can now close this window!<h2></b>");
 ?>
 </pre>
