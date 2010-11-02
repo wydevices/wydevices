7863a8e384f6bda2a41d0a8e976627c63b5223c3
roberto.clemente.bustamante <roberto.clemente.bustamante@968cd671-5f78-0f86-1f23-c28cc3e12018>
2010-11-02 08:20:41 +0000

    corrected path

--- packages/wymodcp-php/web-files/scripts/php/helpusserver.php
+++ packages/wymodcp-php/web-files/scripts/php/helpusserver.php
@@ -1,18 +1,6 @@
-<html>
-
-  <head>
-    <!-- charset must remain utf-8 to be handled properly by Processing -->
-        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-
-    <title>wydevware</title>
-
-
-  </head>
-
-  <body>
         <img src="http://foro.wydev.es/download/file.php?avatar=54_1284403959.png"></img>
         <h1><strong>News:</strong></h1><h3> Last available firmware: <a href="http://foro.wydev.es/viewtopic.php?f=20&t=366">3.1</a><br>
-         Last update released version: <a href="./updates/R249.tar.gz">r249</a></h3><b>R249 Description:</b>
+         Last update released version: <a href="http://foro.wydev.es/wydevware/updates/r249.tar.gz">r249</a></h3><b>R249 Description:</b>
 
 <small>Corrected TV Channel bugfixes and added improvements. Amazing records control panel, more info at home, new cloud wydevware integration... stills working in progress!!</small>
 <br>    <a href="http://foro.wydev.es/wydevware/ViewTargetsStatistics.php"> View Statistics! </a>
@@ -21,8 +9,3 @@
 <hr>
 <a href="./currentvm/Wybox-OVF-v0.1.zip">Download Wybox Virtual Machine</a>
 <hr>
-
-
-
-  </body>
-</html>
