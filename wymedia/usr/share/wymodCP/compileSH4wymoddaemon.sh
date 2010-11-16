f0a02db86c07ddf284dd493e889fc0a694e0144e
roberto.clemente.bustamante <roberto.clemente.bustamante@968cd671-5f78-0f86-1f23-c28cc3e12018>
2010-11-16 09:28:43 +0000

    Not needed anymore

--- wymedia/usr/share/wymodCP/compileSH4wymoddaemon.sh
+++
@@ -1,1 +0,0 @@
-/ZTV/opt/crosstool/gcc-4.1.1-glibc-2.6.1/sh4-unknown-linux-gnu/bin/sh4-unknown-linux-gnu-gcc-4.1.1 -Wall /ZTV/SVN/wydevices/wymodCP/source/wymod.c /ZTV/SVN/wydevices/wymodCP/source/mongoose.c -ldl -lpthread -o /ZTV/SVN/wydevices/wymodCP/wymoddaemon
