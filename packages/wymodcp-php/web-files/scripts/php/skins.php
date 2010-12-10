6df813a389495d5984b276f67afda84732030060
centrena <centrena@968cd671-5f78-0f86-1f23-c28cc3e12018>
2010-12-10 21:47:13 +0000

    Modified skinops.sh and skins.php to enhance the output of the Skins tab

--- packages/wymodcp-php/web-files/scripts/php/skins.php
+++ packages/wymodcp-php/web-files/scripts/php/skins.php
@@ -9,20 +9,20 @@
 <tr><td>
 
 <table>
-<tr><td><input id="flashmod" name="skinops" type="submit" class="wydevradio" value="f"  <?php echo"onclick=\"Skinops('skinops=f')\"";?>/></td><td>Flash Mod</td></tr>
-<tr><td><input id="skinwyplay" name="skinops" type="submit" class="wydevradio" value="s" <?php echo"onclick=\"Skinops('skinops=s')\"";?>/></td><td>System Skin</td></tr>
-<tr><td><input id="skinmod" name="skinops" type="submit" class="wydevradio" value="m" <?php echo"onclick=\"Skinops('skinops=m')\"";?>/></input></td><td>Mod Skin</td></tr>
-<tr><td><input id="export" name="skinops" type="submit" class="wydevradio" value="c" <?php echo"onclick=\"Skinops('skinops=c')\"";?>/></td><td>Export Skin to /wymedia/usr/share/skins/modskin.tar.gz</td></tr>
-<tr><td><input id="import" name="skinops" type="submit" class="wydevradio" value="e" <?php echo"onclick=\"Skinops('skinops=e')\"";?>/></td><td>Import Skin from /wymedia/usr/share/skins/modskin.tar.gz</td></tr>
-<tr><td><input id="exportimgpck" name="skinops" type="submit" class="wydevradio" value="ei" <?php echo"onclick=\"Skinops('skinops=ei')\"";?>/></td><td>export imagepack to /wymedia/usr/share/imagepacks/</td></tr>
-<tr><td><input id="importimgpck" name="skinops" type="submit" class="wydevradio" value="ii" <?php echo"onclick=\"Skinops('skinops=ii')\"";?>/></td><td>import imagepack from /wymedia/usr/share/imagepacks/</td></tr>
+<tr><td><input id="flashmod" name="skinops" type="submit" class="wydevradio" value="f"  <?php echo"onclick=\"Skinops('skinops=f')\"";?>/></td><td>Flash mod</td></tr>
+<tr><td><input id="skinwyplay" name="skinops" type="submit" class="wydevradio" value="s" <?php echo"onclick=\"Skinops('skinops=s')\"";?>/></td><td>System skin</td></tr>
+<tr><td><input id="skinmod" name="skinops" type="submit" class="wydevradio" value="m" <?php echo"onclick=\"Skinops('skinops=m')\"";?>/></input></td><td>Mod skin</td></tr>
+<tr><td><input id="export" name="skinops" type="submit" class="wydevradio" value="c" <?php echo"onclick=\"Skinops('skinops=c')\"";?>/></td><td>Export skin to /wymedia/usr/share/skins/modskin.tar.gz</td></tr>
+<tr><td><input id="import" name="skinops" type="submit" class="wydevradio" value="e" <?php echo"onclick=\"Skinops('skinops=e')\"";?>/></td><td>Import skin from /wymedia/usr/share/skins/modskin.tar.gz</td></tr>
+<tr><td><input id="exportimgpck" name="skinops" type="submit" class="wydevradio" value="ei" <?php echo"onclick=\"Skinops('skinops=ei')\"";?>/></td><td>Export imagepack to /wymedia/usr/share/imagepacks/</td></tr>
+<tr><td><input id="importimgpck" name="skinops" type="submit" class="wydevradio" value="ii" <?php echo"onclick=\"Skinops('skinops=ii')\"";?>/></td><td>Import imagepack from /wymedia/usr/share/imagepacks/</td></tr>
 <tr><td><input id="reboot" name="skinops" type="submit" class="wydevradio" value="r" <?php echo"onclick=\"Skinops('skinops=r')\"";?>/></td><td>Reboot skin</td></tr>
 <tr><td><input id="redfiff" name="skinops" type="submit" class="wydevradio" value="d" <?php echo"onclick=\"Skinops('skinops=d')\"";?>/></td><td>Redfiff skin</td></tr>
-<tr><td><input id="themeserialized" name="skinops" type="submit" class="wydevradio" value="t" <?php echo"onclick=\"Skinops('skinops=t')\"";?>/></td><td>Theme Serialized</td></tr>
-<tr><td><input id="nothemeserialized" name="skinops" type="submit" class="wydevradio" value="nt" <?php echo"onclick=\"Skinops('skinops=nt')\"";?>/></td><td>No Theme Serialized</td></tr>
+<tr><td><input id="themeserialized" name="skinops" type="submit" class="wydevradio" value="t" <?php echo"onclick=\"Skinops('skinops=t')\"";?>/></td><td>Theme serialized</td></tr>
+<tr><td><input id="nothemeserialized" name="skinops" type="submit" class="wydevradio" value="nt" <?php echo"onclick=\"Skinops('skinops=nt')\"";?>/></td><td>No theme serialized</td></tr>
 </table>
 
-</td><td>
+
 
 <table>
 <tr><td>
@@ -51,3 +51,5 @@ echo "</pre>";
 
 </form>	 
 </div>
+
+<br><br><br><br><br><br><br>
