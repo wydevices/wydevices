f504e649b7942bc009dc136ffec6041f16781845
centrena@gmail.com <centrena@gmail.com@968cd671-5f78-0f86-1f23-c28cc3e12018>
2011-11-29 10:26:07 +0000

    Added English translation.

--- wyremote/uninstall-wyremote.sh
+++ wyremote/uninstall-wyremote.sh
@@ -7,7 +7,7 @@ rm /wymedia/usr/share/wymodcp/scripts/php/remotecontrol.php
 rm /wymedia/usr/etc/init.d/wyremote
 sed -i '/input.network/d' /etc/local_conf.py
 sync
-echo "Se ha deinstalado wyremote."
-echo "Se reinicia el sistema en 5 segundos."
+echo "Se ha desinstalado wyremote. | wyremote has been uninstalled."
+echo "Se reinicia el sistema en 5 segundos. | The system is restarted in 5 seconds."
 sleep 5
 reboot
