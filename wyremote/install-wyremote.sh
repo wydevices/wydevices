b6136bd6999c6efce56000a3c08889f38a079d18
centrena@gmail.com <centrena@gmail.com@968cd671-5f78-0f86-1f23-c28cc3e12018>
2011-11-30 11:25:17 +0000

    Cosmetic changes.

--- wyremote/install-wyremote.sh
+++ wyremote/install-wyremote.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Uso: sh install-wyremote.sh
+# Usage: sh install-wyremote.sh
+
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=`dirname $SCRIPT`
 
@@ -7,10 +10,10 @@ cd /wymedia
 tar zxf $SCRIPTPATH/wyremote-*.tar.gz
 if [ $? -eq 0 ]; then
 
+	ln -s /wymedia/wyremote/usr/etc/init.d/wyremote /wymedia/usr/etc/init.d/wyremote
 	ln -s /wymedia/wyremote/usr/share/wymodcp/wyremote.html /wymedia/usr/share/wymodcp/wyremote.html
 	ln -s /wymedia/wyremote/usr/share/wymodcp/scripts/php/remotecontrol.php /wymedia/usr/share/wymodcp/scripts/php/remotecontrol.php
   ln -s /wymedia/wyremote/usr/share/wymodcp/style/wyremote.png /wymedia/usr/share/wymodcp/style/wyremote.png
-	ln -s /wymedia/wyremote/usr/etc/init.d/wyremote /wymedia/usr/etc/init.d/wyremote
 	ln -s /wymedia/usr/bin/mongoose /wymedia/wyremote/usr/bin/mongoose
 
 	sed -i '/input.network/d' /etc/local_conf.py
