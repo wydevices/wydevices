082808cfc5fd4902589f38a14865d09e8e184388
centrena <centrena@968cd671-5f78-0f86-1f23-c28cc3e12018>
2011-11-30 11:19:41 +0000

    Moved uninstall-wyremote.sh

--- wyremote/usr/bin/uninstall-wyremote.sh
+++ wyremote/usr/bin/uninstall-wyremote.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-cd /wymedia
+# Uso: sh /wymedia/wyremote/usr/bin/uninstall-wyremote.sh
+# Usage: sh /wymedia/wyremote/usr/bin/uninstall-wyremote.sh
+
 /wymedia/usr/bin/extras stop wyremote
+rm /wymedia/usr/etc/init.d/wyremote
 rm /wymedia/usr/share/wymodcp/wyremote.html
 rm /wymedia/usr/share/wymodcp/scripts/php/remotecontrol.php
-rm /wymedia/usr/etc/init.d/wyremote
+rm /wymedia/usr/share/wymodcp/style/wyremote.png
+rm -rf /wymedia/wyremote
 sed -i '/input.network/d' /etc/local_conf.py
 sync
 echo "Se ha desinstalado wyremote. | wyremote has been uninstalled."
