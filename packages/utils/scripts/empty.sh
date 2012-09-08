640d4412c3217938e516d701f98490bd035e9c0d
argos66@gmail.com <argos66@gmail.com@968cd671-5f78-0f86-1f23-c28cc3e12018>
2012-09-08 06:33:59 +0000

    Rever build.config to r401. Fix permissions on empty.sh (chmod a+x) and add a comment.

--- packages/utils/scripts/empty.sh
+++ packages/utils/scripts/empty.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
+#
+# Usage : empty.sh command
+#
 empty -f -i /tmp/in -o /tmp/out -L /wymedia/usr/var/log/empty_log telnet 127.0.0.1 31337
 empty -s -o /tmp/in "$1\n"
 empty -s -o /tmp/in "DISCONNECT\n"
