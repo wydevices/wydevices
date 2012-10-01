58caa52201f6e3437e9eb8ab787e89d77092c610
centrena@gmail.com <centrena@gmail.com@968cd671-5f78-0f86-1f23-c28cc3e12018>
2012-10-01 23:15:18 +0000

    Move forcibly /wymedia/usr/share/updates/wybox-extras-latest.txt.

--- packages/wymodcp-php/web-files/scripts/php/updatewe.php
+++ packages/wymodcp-php/web-files/scripts/php/updatewe.php
@@ -1,6 +1,6 @@
 <pre>
 <?php
-system ("tar -zxvf /wymedia/usr/share/updates/wybox-extras-*.tar.gz -C /wymedia/; mv /wymedia/usr/share/updates/wybox-extras-latest.txt /wymedia/usr/etc/wydev-mod-updaterelease; rm -f /wymedia/usr/share/updates/wybox-extras-*");
+system ("tar -zxvf /wymedia/usr/share/updates/wybox-extras-*.tar.gz -C /wymedia/; mv -f /wymedia/usr/share/updates/wybox-extras-latest.txt /wymedia/usr/etc/wydev-mod-updaterelease; rm -f /wymedia/usr/share/updates/wybox-extras-*");
 echo "<br><b><h2>You must reboot the system!<h2></b>";
 echo "<br><b><h2>You can now close this window!<h2></b>";
 ?>
