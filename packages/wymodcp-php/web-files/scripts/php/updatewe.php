c3953517bdb493f37658e5c82751471dae13f8de
centrena <centrena@968cd671-5f78-0f86-1f23-c28cc3e12018>
2012-10-01 20:20:09 +0000

    Initial release of wybox automatic update (2).

---
+++ packages/wymodcp-php/web-files/scripts/php/updatewe.php
@@ -0,0 +1,7 @@
+<pre>
+<?php
+system ("tar -zxvf /wymedia/usr/share/updates/wybox-extras-*.tar.gz -C /wymedia/; mv /wymedia/usr/share/updates/wybox-extras-latest.txt /wymedia/usr/etc/wydev-mod-updaterelease; rm -f /wymedia/usr/share/updates/wybox-extras-*");
+echo "<br><b><h2>You must reboot the system!<h2></b>";
+echo "<br><b><h2>You can now close this window!<h2></b>";
+?>
+</pre>
