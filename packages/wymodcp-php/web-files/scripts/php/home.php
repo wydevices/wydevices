d4476ca133d8e5a9681f0d76371c0fd643b58fea
argos66@gmail.com <argos66@gmail.com@968cd671-5f78-0f86-1f23-c28cc3e12018>
2012-10-07 08:12:57 +0000

    Update jquery to 1.8.2 and jquery form plugin to 3.18. Show memory status in Homepage of wymodcp.

--- packages/wymodcp-php/web-files/scripts/php/home.php
+++ packages/wymodcp-php/web-files/scripts/php/home.php
@@ -11,10 +11,28 @@
     <tr><td>Manufacturer: </td><td><b><?php system("cat /proc/wybox/MN")?></td></tr>
     <tr><td>WydevFirm Version: </td><td><b><?php system("cat /wymedia/etc/wydev-mod-version")?></td></tr>
     <tr><td>Bubble Update Version: </td><td><b><?php  system("cat /wymedia/usr/etc/wydev-mod-updaterelease"); ?></td></tr>
-    <tr><td>Modded Target: </td><td><?php system("cat /proc/wybox/WC")?></td></tr>
-    <tr><td>Real Target: </td><td> <?php system("strings /dev/mtd2 |grep WC |cut -d= -f2")?> </td></tr>
-    <tr><td>Serial Number: </td><td> <?php system("cat /proc/wybox/SN")?> </td></tr>
-    <tr><td>Time / Uptime: </td><td> <?php system("uptime |cut -f1 -d,")?> </td></tr>
+    <tr><td>Modded Target: </td><td><?php system("cat /proc/wybox/WC");?></td></tr>
+    <tr><td>Real Target: </td><td> <?php system("strings /dev/mtd2 |grep WC |cut -d= -f2");?> </td></tr>
+    <tr><td>Serial Number: </td><td> <?php system("cat /proc/wybox/SN");?> </td></tr>
+    <tr><td>Time / Uptime: </td><td> <?php system("uptime |cut -f1 -d,");?> </td></tr>
+    <tr><td>Memory: </td><td> <?php 
+      $mem[0] = trim(exec("free |grep Mem |cut -c 15-19"));
+      $mem[1] = trim(exec("free |grep Mem |cut -c 28-32"));
+      $mem_percent = round((intval($mem[1]) / intval($mem[0])) * 100, 0);
+      $mem_color = "#3F3";
+      if ($mem_percent > 90) $mem_color = "orange";
+      if ($mem_percent > 95) $mem_color = "#F33";
+      echo "<b>".$mem[1]."Ko / ".$mem[0]." Ko</b><div id=\"progress-outer\"><div id=\"progress-inner\" style=\"width: ".$mem_percent."%; background: ".$mem_color.";\"></div></div>"
+    ?></td></tr>
+    <tr><td>Swap: </td><td> <?php 
+      $swap[0]= trim(exec("free |grep Swap |cut -c 14-19"));
+      $swap[1]= trim(exec("free |grep Swap |cut -c 27-32"));
+      $swap_percent = round((intval($swap[1]) / intval($swap[0])) * 100, 0);
+      $swap_color = "#3F3";
+      if ($swap_percent > 90) $swap_color = "orange";
+      if ($swap_percent > 95) $swap_color = "#F33";
+      echo "<b>".$swap[1]."Ko / ".$swap[0]." Ko</b><div id=\"progress-outer\"><div id=\"progress-inner\" style=\"width: ".$swap_percent."%; background: ".$swap_color.";\"></div></div>"
+    ?></td></tr>
     <tr><td>Fan Speed: </td><?php 
       $fan_speed = exec("cat /sys/devices/platform/stm-pwm/pwm1 | tr -d");
       echo "<td><img src=\"./style/fan.png\" />&nbsp;".$fan_speed." RPM</td></tr>";
