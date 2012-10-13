cf3e429d284cfa4bedbd28c43192f8e07bcbaafd
centrena@gmail.com <centrena@gmail.com@968cd671-5f78-0f86-1f23-c28cc3e12018>
2012-10-13 13:20:37 +0000

    updatewe.php shows message on the browser window.

--- packages/wymodcp-php/web-files/scripts/php/updatewe.php
+++ packages/wymodcp-php/web-files/scripts/php/updatewe.php
@@ -2,8 +2,7 @@
 <?php
 system ("tar -zxvf /wymedia/tmp/wybox-extras-*.tar.gz -C /wymedia/tmp; sh /wymedia/tmp/install.sh; mv -f /wymedia/tmp/wybox-extras-latest.txt /wymedia/usr/etc/wydev-mod-updaterelease; rm -f /wymedia/tmp/wybox-extras-*; rm -f /wymedia/tmp/install.sh");
 echo "<br><b><h2>The system will restart in 30 seconds!<h2></b>";
-system ("sleep 30");
-system ("reboot");
 echo "<br><b><h2>You can now close this window!<h2></b>";
+system ("nohup reboot -d30 &");
 ?>
 </pre>
