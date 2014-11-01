30cdc370a03cb60102936a56bf336ef89a5fd2d5
roberto.clemente.bustamante@gmail.com <roberto.clemente.bustamante@gmail.com@968cd671-5f78-0f86-1f23-c28cc3e12018>
2014-11-01 13:39:18 +0000

    WyMod Get Last PHP Sources (From SVN trunk)

---
+++ packages/utils/scripts/wymodgetlastphp.sh
@@ -0,0 +1,8 @@
+abfn=$1
+fn=${abfn##*/} 
+
+echo "Deleting $abfn"
+rm $abfn
+echo "Downlading replacement for Â$fn"
+wget http://wydevices.googlecode.com/svn/trunk/packages/wymodcp-php/web-files/scripts/php/$fn -O $abfn
+
