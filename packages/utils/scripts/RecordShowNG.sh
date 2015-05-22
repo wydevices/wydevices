2554da65c21dd5f01f1bcca5e542139ed9187343
wydevices <wydevices@users.noreply.github.com>
2015-05-22 20:52:00 +0200

    Update RecordShowNG.sh
    add new code!

--- packages/utils/scripts/RecordShowNG.sh
+++ packages/utils/scripts/RecordShowNG.sh
@@ -4,9 +4,10 @@
 # $2 Duration
 # $3 Station
 # $4 SingleOutfile
+# $5 Default Pic at /wymedia/Music/WYRADIO/PICS/
 
-if [ "$#" -ne 4 ] ; then
-	echo "Usage: RecordShowNG.sh <ShowName> <Duration> <Station> <SingleOutfile>";
+if [ "$#" -lt 4 ] ; then
+	echo "Usage: RecordShowNG.sh <ShowName> <Duration> <Station> <SingleOutfile><DefaultPicPath>";
 	echo "";
 	echo "Possible Stations (Stream Sources) are:";
 	echo `sqlite3 /wymedia/.wyradio/wyradio.db3 "SELECT acronym FROM streamsources"`;
@@ -19,7 +20,7 @@ else
 
 	URL=`sqlite3 /wymedia/.wyradio/wyradio.db3 "SELECT url FROM streamsources WHERE [acronym] = '$3'"`;
 	OUTFOLDER=`sqlite3 /wymedia/.wyradio/wyradio.db3 "SELECT outfolder FROM streamsources WHERE [acronym] = '$3'"`;
-	OUTFILE=$1_$(date "+%d-%m-%Y_%H-%M");
+	OUTFILE=$(date "+%Y%m%d_%H-%M")_$1;
 
 	if [ $4 -eq 0 ] ; then
 		SINGLE="";
@@ -39,6 +40,16 @@ else
 
 	streamripper $URL $SINGLE $OUTFILE -l $2;
 
+	id3v2 -A $1 $OUTFILE.mp3
+	id3v2 -a $1 $OUTFILE.mp3
+	id3v2 -t $OUTFILE $OUTFILE.mp3
+	
+	#id3v2 --TIT2 FECHANOMBRE  $OUTFILE.mp3
+	#id3v2 --TPE1 DJ $OUTFILE.mp3
+	#id3v2 --TALB PROGRAM $OUTFILE.mp3
+	id3v2 --TYER 2015 $OUTFILE.mp3
+	id3v2 --TCON 52 $OUTFILE.mp3
+
+	
+
 fi
-
-
