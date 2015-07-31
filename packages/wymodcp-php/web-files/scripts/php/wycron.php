c16688effbcb60e48e1ecf4ef0d0406c5058f6c3
centrena <centrena@gmail.com>
2015-07-31 03:26:39 +0200

    Updated wycron.php: reorganizing output messages.

--- packages/wymodcp-php/web-files/scripts/php/wycron.php
+++ packages/wymodcp-php/web-files/scripts/php/wycron.php
@@ -18,9 +18,6 @@
 //
 // Freedom to the Wy!
 
-// Create /wymedia/Music symlink.
-system("if [ ! -d /wymedia/Music ]; then ln -s '/wymedia/My Music' /wymedia/Music; fi");
-
 // File containing cronjobs
 $cronFile = '/wymedia/usr/etc/cron.d/root';
 
@@ -55,7 +52,102 @@ function cron_edit($cronFile) {
 <body style="font-family: Verdana, sans-serif; font-size: x-small">
 
 <h1>WyCron / WyRadio</h1>
+
 <hr>
+
+<?php
+
+	if (!file_exists ("/wymedia/Music")) {
+		echo "Creating /wymedia/Music symlink.<br><br>";
+		system("ln -s '/wymedia/My Music' /wymedia/Music");
+	}
+	if (!file_exists ("/wymedia/.wyradio/wyradio.db3")) {
+		echo "Creating DB: /wymedia/.wyradio/wyradio.db3.<br><br>";
+		system ("mkdir /wymedia/.wyradio/");
+		system ("cat /wymedia/usr/share/wymodcp/scripts/sql/CreateEmptyWyradioDB.sql | sqlite3 /wymedia/.wyradio/wyradio.db3");
+	}
+	if (!file_exists ("/wymedia/usr/share/wymodcp/WYRADIO")) {
+		echo "Creating root WYRADIO: /wymedia/usr/share/wymodcp/WYRADIO.<br><br>";
+		system ("mkdir /wymedia/usr/share/wymodcp/WYRADIO");
+	}
+
+	$dbfile = new PDO('sqlite:/wymedia/.wyradio/wyradio.db3');
+
+	if (isset($_GET['addstream'])) {
+		//form submit code here
+		$asname = $_GET['name'];
+		$asacronym = $_GET['acronym'];
+		$asstreamurl = $_GET['streamurl'];
+		$asoutfolder = $_GET['outfolder'];
+		$SQLAdd = "INSERT INTO streamsources (name, acronym, url, outfolder) VALUES ('".$asname."', '".$asacronym."', '".$asstreamurl."', '".$asoutfolder."');";
+		echo "<br>";
+		echo "SQL command: <i>".$SQLAdd."</i><br><br>";
+		$dbfile->exec($SQLAdd);
+		echo "Adding new stream.<br>";
+		echo "Name: <i>".$asname."</i>";
+		echo "<br>";
+		echo "Acronym: <i>".$asacronym."</i>";
+		echo "<br>";
+		echo "Stream URL: <i>".$asstreamurl."</i>";
+		echo "<br>";
+		echo "Out Folder: <i>".$asoutfolder."</i>";
+		echo "<br><br>";
+	}
+
+	if (isset($_GET['delstreamcount'])) {
+		echo "<br>";
+		for ($acrcount = 0; $acrcount < $_GET['delstreamcount']; $acrcount++) {
+			$delthisid = "ID".$acrcount;
+			$delthis = $_GET[$delthisid];
+			$SQLDelete = "DELETE FROM streamsources WHERE acronym='".$delthis."';";
+			echo "SQL command: <i>".$SQLDelete."</i><br><br>";
+			$dbfile->exec($SQLDelete);
+			echo "Deleting stream <i>".$delthis."</i>.<br>";
+		}
+		echo "<br>";
+	}
+
+	if (isset($_GET['addshow'])) {
+		//form submit code here
+		$asname = $_GET['name'];
+		$asstreamsourceid = $_GET['streamsource'];
+		$ashour = $_GET['hour'];
+		$asminute = $_GET['minute'];
+		$asweekday = $_GET['weekday'];
+		$asmonth = $_GET['month'];
+		$asmonthday = $_GET['monthday'];
+		$asoutsinglefile = $_GET['singlefile'];
+		if ($asoutsinglefile == "true"):
+			$asoutsinglefile = 1;
+		else:
+			$asoutsinglefile = 0;
+		endif;
+		$asduration = $_GET['duration'];
+		$asacronym = substr (str_replace(" ","",$asname),0,9);
+		$insertsql = "INSERT INTO shows ([name], [acronym], [streamsourceid], [duration], [outsinglefile], [minute], [hour], [monthday], [weekday], [month]) VALUES ('".$asname."', '".$asacronym."', '".$asstreamsourceid."', ".$asduration.", '".$asoutsinglefile."', ".$asminute.", ".$ashour.", ".$asmonthday.", ".$asweekday.", ".$asmonth.");";
+		echo "<br>";
+		echo "SQL command: <i>".$insertsql."</i><br><br>";
+		$dbfile->exec($insertsql);
+		echo "Adding new show.<br>";
+		echo "Name: <i>".$asname."</i>";
+		echo "<br>";
+		echo "Stream: <i>".$asstreamsourceid."</i>";
+		echo "<br><br>";
+	}
+
+	if (isset($_GET['delshowcount'])) {
+		for ($acrcount = 0; $acrcount < $_GET['delshowcount']; $acrcount++) {
+			$delthisid = "ID".$acrcount;
+			$delthis = $_GET[$delthisid];
+			$SQLDelete = "DELETE FROM shows WHERE acronym='".$delthis."';";
+			echo "SQL command: <i>".$SQLDelete."</i><br><br>";
+			$dbfile->exec($SQLDelete);
+			echo "Deleting show <i>".$delthis."</i>.<br>";
+		}
+		echo "<br>";
+	}
+
+?>
 
 <h2>Explorer<input type="button" onClick="$('#jqftwyradio2').slideToggle();" class="wydevslidebutton"/></input></h2>
 <div class="jqft">
@@ -67,33 +159,9 @@ function cron_edit($cronFile) {
 
 <?php
 
-	if (!file_exists ("/wymedia/.wyradio/wyradio.db3")):
-		echo "Creating DB";
-		system ("mkdir /wymedia/.wyradio/");
-		system ("cat /wymedia/usr/share/wymodcp/scripts/sql/CreateEmptyWyradioDB.sql | sqlite3 /wymedia/.wyradio/wyradio.db3");
-	endif;
-	
-	if (!file_exists ("/wymedia/usr/share/wymodcp/WYRADIO/")):
-		echo "Creating root WYRADIO";
-		system ("mkdir /wymedia/usr/share/wymodcp/WYRADIO/");
-	endif;
-	
-	$dbfile = new PDO('sqlite:/wymedia/.wyradio/wyradio.db3');
-	
 	$selectsql = 'SELECT * FROM streamsources';
-	
 	echo "<form name='deletestream' action='wycron.php' method='POST'>";
 	echo "<table>";
-	if (isset($_GET['delstreamcount'])) {
-		echo "SQL Syntax:";
-		for ($acrcount = 0; $acrcount < $_GET['delstreamcount']; $acrcount++) {
-			$delthisid = "ID".$acrcount;
-			$delthis = $_GET[$delthisid];
-			$SQLDelete = "DELETE FROM streamsources WHERE acronym='".$delthis."';";
-			echo $SQLDelete."<br>";
-			$dbfile->exec($SQLDelete);
-		}
-	}
 	foreach ($dbfile->query($selectsql) as $returnrow) {
 		$name = $returnrow['name'];
 		$acronym = $returnrow['acronym'];
@@ -101,14 +169,14 @@ function cron_edit($cronFile) {
 		$streamurl = $returnrow['url'];
 		if (file_exists ($path)):
 			if (!file_exists ("/wymedia/usr/share/wymodcp/WYRADIO/".$acronym."/")):
-				echo "Creating symlink for:".$path."<br>";
+				echo "Creating symlink for <i>".$path."</i>.<br>";
 				system("ln -s ".$path." /wymedia/usr/share/wymodcp/WYRADIO/".$acronym);
 			endif;
 		else:
-			echo "Creating folder:".$path."<br>";
+			echo "Creating folder <i>".$path."</i>.<br>";
 			system("mkdir -p ".$path);
 			if (!file_exists ("/wymedia/usr/share/wymodcp/WYRADIO/".$acronym)):
-				echo "Creating symlink for:".$path."<br>";
+				echo "Creating symlink for <i>".$path."</i>.<br>";
 				system("ln -s ".$path." /wymedia/usr/share/wymodcp/WYRADIO/".$acronym);
 			endif;
 		endif;
@@ -119,7 +187,7 @@ function cron_edit($cronFile) {
 		echo "<td><input type=checkbox name=deletestream[] value=".$acronym.">";
 		echo "</td></tr>";
 	}
-	echo "<tr><td><input id='deletebutton' name='deletebutton' class='button' type='button' onclick='DeleteStream();' value='Delete Stream'/></td></tr>";
+	echo "<tr><td><input id='deletebutton' name='deletebutton' class='button' type='button' onclick='DeleteStream();' value='Delete stream'/></td></tr>";
 	echo "</table>";
 	echo "</form>";
 
@@ -129,76 +197,23 @@ function cron_edit($cronFile) {
 
 <h2>Add new stream<input type="button" onClick="$('#addstream').slideToggle();" class="wydevslidebutton"/></input></h2>
 <div id="addstream" name="addstream" style="display:none;">
-
-<?php
-
-	if (isset($_GET['addstream'])) {
-		//form submit code here
-		$asname = $_GET['name'];
-		$asacronym = $_GET['acronym'];
-		$asstreamurl = $_GET['streamurl'];
-		$asoutfolder = $_GET['outfolder'];
-		$dbfile->exec("INSERT INTO streamsources (name, acronym, url, outfolder ) VALUES ('".$asname."', '".$asacronym."', '".$asstreamurl."', '".$asoutfolder."' );");
-		echo $asname;
-		echo "<br>";
-		echo $asacronym;
-		echo "<br>";
-		echo $asstreamurl;
-		echo "<br>";
-		echo $asoutfolder;
-		echo "<br>";
-	} else {
-
-?>
-
 <form Name="addstream" action="./scripts/php/wycron.php" method="post">
-Name: <input type="text" name="name" value="" />
-Acronym: <input type="text" name="acronym" value="" />
+Name: <input type="text" name="name" value=""/>
+Acronym: <input type="text" name="acronym" value=""/>
 <br>
-Stream URL: <input type="text" name="streamurl" value="" size="75" /> </br>
-Out Folder: <input type="text" name="outfolder" value="/wymedia/Music/WYRADIO/" />
-<input id="addstream" name="addstream" class="button" type="button" onclick="AddStream();" value="Add Stream"/>
+Stream URL: <input type="text" name="streamurl" value="" size="75"/> </br>
+Out Folder: <input type="text" name="outfolder" value="/wymedia/Music/WYRADIO/"/>
+<input id="addstream" name="addstream" class="button" type="button" onclick="AddStream();" value="Add stream"/>
 </form>
-
-<?php
-
-	}
-	// end of form
-
-?>
-
 </div>
 
 <h2>Shows<input type="button" onClick="$('#shows').slideToggle();" class="wydevslidebutton"/></input></h2>
-
 <div id="shows" name="shows" style="display:none;">
-<pre>
 
 <?php
-
-	if (isset($_GET['delshowcount'])) {
-		echo "SQL Syntax:";
-		for ($acrcount = 0; $acrcount < $_GET['delshowcount']; $acrcount++) {
-			$delthisid = "ID".$acrcount;
-			$delthis = $_GET[$delthisid];
-			$SQLDelete = "DELETE FROM shows WHERE acronym='".$delthis."';";
-			echo $SQLDelete."<br>";
-			$dbfile->exec($SQLDelete);
-		}
-	}
-
-?>
-
-</pre>
-
-<form name='deleteshow' action='wycron.php' method='POST'>
-
-<?php
-
-	$dbfile = new PDO('sqlite:/wymedia/.wyradio/wyradio.db3');
 	
 	$selectsql = 'SELECT * FROM shows';
-	
+	echo "<form name='deleteshow' action='wycron.php' method='POST'>";
 	foreach ($dbfile->query($selectsql) as $returnrow) {
 		$name = $returnrow['name'];
 		$acronym = $returnrow['acronym'];
@@ -234,46 +249,20 @@ Out Folder: <input type="text" name="outfolder" value="/wymedia/Music/WYRADIO/" 
 		endif;
 		echo "<input type=checkbox name=deleteshow[] value=".$acronym.">".$minute." ".$hour." ".$monthday." ".$month." ".$weekday." RecordShowNG.sh ".str_replace(" ","_",$name)." ".$duration." ".$streamid." ".$outsinglefile." ".$defaultpic." </br>" ;
 	}
+	echo "<input id='deleteshowbutton' name='deleteshowbutton' class='button' type='button' onclick='DeleteShow();' value='Delete show'/>";
+	echo "</form>";
 
 ?>
 
-<input id='deleteshowbutton' name='deleteshowbutton' class='button' type='button' onclick='DeleteShow();' value='DeleteShow'/>
-</form>
 </div>
 
-<h2> Add Shows <input type="button" onClick="$('#addshow').slideToggle();" class="wydevslidebutton"/></input></h2>
+<h2>Add new show <input type="button" onClick="$('#addshow').slideToggle();" class="wydevslidebutton"/></input></h2>
 <div id="addshow" name="addshow" style="display:none;">
-
-<?php
-	if (isset($_GET['addshow'])) {
-		//form submit code here
-		$asname = $_GET['name'];
-		$asstreamsourceid = $_GET['streamsource'];
-		$ashour = $_GET['hour'];
-		$asminute = $_GET['minute'];
-		$asweekday = $_GET['weekday'];
-		$asmonth = $_GET['month'];
-		$asmonthday = $_GET['monthday'];
-		$asoutsinglefile = $_GET['singlefile'];
-		if ($asoutsinglefile == "true"):
-			$asoutsinglefile = 1;
-		else:
-			$asoutsinglefile = 0;
-		endif;
-		$asduration = $_GET['duration'];
-		$asacronym = substr (str_replace(" ","",$asname),0,9);
-		$insertsql = "INSERT INTO shows ([name],[acronym],[streamsourceid],[duration],[outsinglefile],[minute],[hour],[monthday],[weekday],[month]) VALUES ('".$asname."', '".$asacronym."', '".$asstreamsourceid."', ".$asduration.", '".$asoutsinglefile."',".$asminute.",".$ashour.",".$asmonthday.",".$asweekday.",".$asmonth." );";
-		echo $insertsql;
-		$dbfile->exec($insertsql);
-	} else {
-
-?>
-
 <form Name="addshow" action="./scripts/php/wycron.php" method="post">
 <table>
-<tr><td>Nombre</td><td>Emisora</td><td>Hora</td><td>Minuto</td><td>Dia del Mes</td><td>Mes</td><td>Dia de la Semana</td><td>Duracion</td><td>Archivo Unico</td></tr>
+<tr><td>Name</td><td>Stream</td><td>Hour</td><td>Minute</td><td>Day of the month</td><td>Month</td><td>Weekday</td><td>Duration</td><td>Single file</td></tr>
 <tr><td>
-<input type="text" name="showname" value="" />
+<input type="text" name="showname" value=""/>
 </td><td>
 <select name="streamsource">
 
@@ -291,7 +280,7 @@ Out Folder: <input type="text" name="outfolder" value="/wymedia/Music/WYRADIO/" 
 </select>
 </td><td>
 <select name="hour">
-<option value="null">Todos</option>
+<option value="null">All</option>
 
 <?php
 
@@ -302,7 +291,7 @@ Out Folder: <input type="text" name="outfolder" value="/wymedia/Music/WYRADIO/" 
 </select>
 </td><td>
 <select name="minute">
-<option value="null">Todos</option>
+<option value="null">All</option>
 
 <?php
 
@@ -313,7 +302,7 @@ Out Folder: <input type="text" name="outfolder" value="/wymedia/Music/WYRADIO/" 
 </select>
 </td><td>
 <select name="monthday">
-<option value="null">Todos</option>
+<option value="null">All</option>
 
 <?php
 
@@ -324,50 +313,44 @@ Out Folder: <input type="text" name="outfolder" value="/wymedia/Music/WYRADIO/" 
 </select>
 </td><td>
 <select name="month">
-<option value="null">Todos</option>
-<option value="1">Enero</option>
-<option value="2">Febrero</option>
-<option value="3">Marzo</option>
-<option value="4">Abril</option>
-<option value="5">Mayo</option>
-<option value="6">Junio</option>
-<option value="7">Julio</option>
-<option value="8">Agosto</option>
-<option value="9">Septiembre</option>
-<option value="10">Octubre</option>
-<option value="11">Noviembre</option>
-<option value="12">Diciembre</option>
+<option value="null">All</option>
+<option value="1">January</option>
+<option value="2">February</option>
+<option value="3">March</option>
+<option value="4">April</option>
+<option value="5">May</option>
+<option value="6">June</option>
+<option value="7">July</option>
+<option value="8">August</option>
+<option value="9">September</option>
+<option value="10">October</option>
+<option value="11">November</option>
+<option value="12">December</option>
 </select>
 </td><td>
 <select name="weekday">
-<option value="null">Todos</option>
-<option value="1">Lunes</option>
-<option value="2">Martes</option>
-<option value="3">Miercoles</option>
-<option value="4">Jueves</option>
-<option value="5">Viernes</option>
-<option value="6">Sabado</option>
-<option value="0">Domingo</option>
+<option value="null">All</option>
+<option value="1">Monday</option>
+<option value="2">Tuesday</option>
+<option value="3">Wednesday</option>
+<option value="4">Thursday</option>
+<option value="5">Friday</option>
+<option value="6">Saturday</option>
+<option value="0">Sunday</option>
 </select>
 </td><td>
 <input type="text" name="duration" default="3600" size="6">
 </td><td>
 <input type="checkbox" name="singlefile" default="1">
 </td>
-<tr><td><input id="addshow" name="addshow" class="button" type="button" onclick="AddShow();" value="Add Show"/></td></tr>
+<tr><td><input id="addshow" name="addshow" class="button" type="button" onclick="AddShow();" value="Add show"/></td></tr>
 </table>
 </form>
-
-<?php
-
-	}
-	// end of form
-
-?>
-
 </div>
 
 <hr>
+
+<h2>IceS</h2>
 
 <?php
 
@@ -384,8 +367,9 @@ Out Folder: <input type="text" name="outfolder" value="/wymedia/Music/WYRADIO/" 
 		$startfolder = $_GET['startfolder'];
 		$action = $_GET['action'];
 		if ($action == "start") {
-			$cmd = 'ices -h '.$address.' -m /'.$mountpoint.' -P \''.$mountpwd.'\' -F '.$playlist.' -t http -p '.$port.' -v -b 128 -n '.$name.' -g '.$description.' -i -B' ;
-			echo $cmd;
+			$cmd = 'ices -h '.$address.' -m /'.$mountpoint.' -P \''.$mountpwd.'\' -F '.$playlist.' -t http -p '.$port.' -v -b 128 -n '.$name.' -g '.$description.' -i -B';
+			echo "ices -h ".$address." -m /".$mountpoint." -P ****** -F ".$playlist." -t http -p ".$port." -v -b 128 -n ".$name." -g ".$description." -i -B";
+			echo "<br><br>";
 			exec($cmd);
 		}
 		if ($action == "next") {
@@ -401,28 +385,37 @@ Out Folder: <input type="text" name="outfolder" value="/wymedia/Music/WYRADIO/" 
 			system("mkdir -p /wymedia/icesconf");
 			$cmd = '/wymedia/usr/bin/find '.$startfolder.' -name '.$filter.' > '.$playlist ;
 			echo $cmd;
+			echo "<br><br>";
 			system($cmd);
 		}
 	}
 
 ?>
 
-<form Name="icesdata" method="GET" action="./scripts/php/wycron.php">
+<form name="icesdata" method="GET" action="./scripts/php/wycron.php">
 <table>
-<tr><td>
-Server Address: </td><td> <input type="text" name="address" value="w.x.y.z" /> </td><td>
-Mount Point: </td><td> <input type="text" name="mountpoint" value="stream" /> </td><td>
-Server Port: </td><td> <input type="text" name="port" value="8000" /> </td></tr><tr><td>
-Mount Password: </td><td> <input type="password" name="mountpwd" value="" /> </td><td>
-Station Name: </td><td> <input type="text" name="name" value="WyRadio" /> </td><td>
-Stream Description: </td><td> <input type="text" name="description" value="WyRadio now streaming" /> </td></tr><tr><td>
-</tr><tr> </tr><tr> </tr><tr><td>
-Playlist file: </td><td> <input type="text" name="playlist" value="/wymedia/icesconf/playlist.txt" size=30 /> </td></tr><tr><td>
-Folder: </td><td> <input type="text" name="startfolder" value="/wymedia/Music/WYRADIO/IGR/" size=30 /> </td></tr><tr><td>
-Name filter: </td><td> <input type="text" name="filter" value="*2015*.mp3" size=30 /> </td></tr>
+<tr>
+<td>Server Address: </td><td> <input type="text" name="address" value="10.0.0.1"/> </td>
+<td>Mount Point: </td><td> <input type="text" name="mountpoint" value="stream"/> </td>
+<td>Server Port: </td><td> <input type="text" name="port" value="8000"/> </td>
+</tr>
+<tr>
+<td>Mount Password: </td><td> <input type="password" name="mountpwd" value=""/> </td>
+<td>Station Name: </td><td> <input type="text" name="name" value="WyRadio"/> </td>
+<td>Stream Description: </td><td> <input type="text" name="description" value="WyRadio now streaming"/> </td>
+</tr>
+<tr>
+<td>Playlist File: </td><td> <input type="text" name="playlist" value="/wymedia/icesconf/playlist.txt" size=30 /> </td>
+</tr>
+<tr>
+<td>Folder: </td><td> <input type="text" name="startfolder" value="/wymedia/Music/WYRADIO/" size=30 /> </td>
+</tr>
+<tr>
+<td>Name Filter: </td><td> <input type="text" name="filter" value="*2015*.mp3" size=30 /> </td>
+</tr>
 </table>
 <p> 
-<input id="startices" name="startices" class="button" type="button" onclick="IcesHandler('start');" value="Start Ices"/>
+<input id="startices" name="startices" class="button" type="button" onclick="IcesHandler('start');" value="Start IceS"/>
 <input id="nextices" name="nextices" class="button" type="button" onclick="IcesHandler('next');" value="Next"/>
 <input id="killices" name="killices" class="button" type="button" onclick="IcesHandler('kill');" value="Kill"/>
 <input id="reloadices" name="reloadices" class="button" type="button" onclick="IcesHandler('reload');" value="Reload playlist"/>
@@ -452,13 +445,13 @@ Name filter: </td><td> <input type="text" name="filter" value="*2015*.mp3" size=
 <b>Current Date & Time: </b> <?php system('date');?>
 
 ###################################################################
---------------- minuto (0 - 59) 
-|  .------------- hora (0 - 23)
-|  |  .---------- día del mes (1 - 31)
-|  |  |  .------- mes (1 - 12) O jan,feb,mar,apr ... (los meses en inglés)
-|  |  |  |  .---- día de la semana (0 - 6) (Domingo=0 ó 7) O sun,mon,tue,wed,thu,fri,sat (los días en inglés) 
+--------------- minute (0 - 59) 
+|  .------------- hour (0 - 23)
+|  |  .---------- day of the month (1 - 31)
+|  |  |  .------- month (1 - 12) or jan,feb,mar,apr...
+|  |  |  |  .---- weekday (0 - 6) (Sunday=0 or 7) or sun,mon,tue,wed,thu,fri,sat 
 |  |  |  |  |
-*  *  *  *  *  comando para ser ejecutado
+*  *  *  *  *  command to be executed
 
 </pre>
 
@@ -511,6 +504,7 @@ Name filter: </td><td> <input type="text" name="filter" value="*2015*.mp3" size=
 
 		system ("/wymedia/usr/etc/init.d/crond stop");
 		system ("/wymedia/usr/etc/init.d/crond start");
+		echo "<br><br>";
 	else:
 		//echo "nodata";
 	endif;
@@ -529,7 +523,7 @@ Name filter: </td><td> <input type="text" name="filter" value="*2015*.mp3" size=
 ?>
 
 </textarea>
-<input id="updatecron" name="updatecron" class="button" type="button" onclick="WyCron(document.wycron.crontab.value)" value="Update Crontab"/>
+<input id="updatecron" name="updatecron" class="button" type="button" onclick="WyCron(document.wycron.crontab.value)" value="Update crontab"/>
 </div>
 </form>
             
