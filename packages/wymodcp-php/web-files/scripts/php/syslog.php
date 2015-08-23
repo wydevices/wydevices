168fae534029f46362dfb432004c13cdc0493ec4
Wydevices <wydevices@users.noreply.github.com>
2015-08-23 19:02:58 +0200

    Update syslog.php
    Añadir los logs de acceso y error de mongoose y el de transmission

--- packages/wymodcp-php/web-files/scripts/php/syslog.php
+++ packages/wymodcp-php/web-files/scripts/php/syslog.php
@@ -4,9 +4,30 @@ echo "<dl><dd>Show the 100 last lines from /wymedia/usr/var/log/syslog.log</dl>"
 echo "<textarea rows=\"101\" cols=\"130\" wrap=\"off\" readonly>";
 system("tail -n100 /wymedia/usr/var/log/syslog.log");
 echo "</textarea>";
+
 echo "<h1>mediatomb</h1>";
 echo "<dl><dd>Show the 100 last lines from /wymedia/usr/var/log/mediatomb.log</dl>";
 echo "<textarea rows=\"101\" cols=\"130\" wrap=\"off\" readonly>";
 system("tail -n100 /wymedia/usr/var/log/mediatomb.log");
 echo "</textarea>";
+
+echo "<h1>Transmission</h1>";
+echo "<dl><dd>Show the 100 last lines from /wymedia/usr/var/log/transmission.log</dl>";
+echo "<textarea rows=\"101\" cols=\"130\" wrap=\"off\" readonly>";
+system("tail -n100 /wymedia/usr/var/log/transmission.log");
+echo "</textarea>";
+
+echo "<h1>echo "<h1>Mongoose access</h1>";
+echo "<dl><dd>Show the 100 last lines from /wymedia/usr/var/log/mongoose_access.log</dl>";
+echo "<textarea rows=\"101\" cols=\"130\" wrap=\"off\" readonly>";
+system("tail -n100 /wymedia/usr/var/log/mongoose_access.log");
+echo "</textarea>";
+
+
+echo "<h1>echo "<h1>Mongoose error</h1>";
+echo "<dl><dd>Show the 100 last lines from /wymedia/usr/var/log/mongoose_error.log</dl>";
+echo "<textarea rows=\"101\" cols=\"130\" wrap=\"off\" readonly>";
+system("tail -n100 /wymedia/usr/var/log/mongoose_error.log");
+echo "</textarea>";
+
 ?>
