aadb2815817f32c5189e1aa1659d3ffb936372f7
Wydevices <wydevices@users.noreply.github.com>
2015-08-30 11:07:09 +0200

    Update docs.php
    integrar con load de jquery los documentos html

--- packages/wymodcp-php/web-files/scripts/php/docs.php
+++ packages/wymodcp-php/web-files/scripts/php/docs.php
@@ -1,11 +1,29 @@
+
 <div id="docs" class="docscontainer">
 <h1>Help Documents</h1><br />
+
 <?php
 $dir = "/wymedia/usr/share/wymodcp/docs/"; 
 if (is_dir($dir)) {
   if ($dh = opendir($dir)) {
     while (($file = readdir($dh)) !== false) {
-      if ($file != "." && $file != "..") echo "<a href=\"./docs/".$file."\">".$file."</a><br>\n";
+      if ($file != "." && $file != "..") {
+
+	if (strstr($file, ".html")){
+
+$fileid = substr($file, 0, 5);
+
+echo "<h3> ".$file."<input type=\"button\" onClick=\"$('#".$fileid."').load( '/docs/".$file."'
+); $('#".$fileid."').slideToggle() ;\" class=\"wydevslidebutton\"/></input></h3>";
+	echo "<div id=\"". $fileid."\" name=\"".$fileid."\" style=\"display:none;\">";
+	echo "</div>";
+
+	}
+	else
+	{
+		echo "<a href=\"./docs/".$file."\">".$file."</a><br>\n";
+	}
+	}
     }
     closedir($dh);
   }
